a7c9dd23fa474bbce27dd85160cb8c0c7256c0ed
karinamintic <karina.arias.mt@correo.usa.edu.co>
2021-09-30 06:32:07 -0500

    Metodo PUT (implementado)
    Se implementa el metodo PUT

--- js/misFunciones.js
+++ js/misFunciones.js
@@ -26,8 +26,35 @@ function pintarRespuesta(items)
         myTable+= "<td>"+items[i].model+"</td>";
         myTable+= "<td>"+items[i].category_id+"</td>";
         myTable+= "</tr>";
-
+        /// id name brand model category_id
     } 
     myTable += "</table>";
     $("#resultado").append(myTable);
+}
+function guardarInformacion()
+{
+    let myData = {
+        id:$("#id").val(),
+        name:$("#name").val(),
+        brand:$("#brand").val(),
+        model:$("#model").val(),
+        category_id:$("#category_id").val(), 
+    }
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"https://g4ef86a70a56228-db202109252211.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/skate/skate",
+        type: "POST",
+        data: myData,
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            $("#id").val("");
+            $("#name").val("");
+            $("#brand").val("");
+            $("#model").val("");
+            $("#category_id").val("");
+            traerInformacion();    
+            alert("Se ha guardado los datos");
+        }
+    });
 }
