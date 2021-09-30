c2c12907eaab4b95813a7bb4963f8a2da0aeeca6
karinamintic <karina.arias.mt@correo.usa.edu.co>
2021-09-30 07:19:53 -0500

    Metodo POST DELETE (implementado)

--- js/misFunciones.js
+++ js/misFunciones.js
@@ -25,6 +25,7 @@ function pintarRespuesta(items)
         myTable+= "<td>"+items[i].brand+"</td>";
         myTable+= "<td>"+items[i].model+"</td>";
         myTable+= "<td>"+items[i].category_id+"</td>";
+        myTable+= "<td><button onclick='borrarElemento("+items[i].id+")'>Borrar</button></td>";
         myTable+= "</tr>";
         /// id name brand model category_id
     } 
@@ -55,6 +56,53 @@ function guardarInformacion()
             $("#category_id").val("");
             traerInformacion();    
             alert("Se ha guardado los datos");
+        }
+    });
+}
+function editarInformacion()
+{
+    let myData = {
+        id:$("#id").val(),
+        name:$("#name").val(),
+        brand:$("#brand").val(),
+        model:$("#model").val(),
+        category_id:$("#category_id").val(), 
+    }
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"https://g4ef86a70a56228-db202109252211.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/skate/skate",
+        type: "PUT",
+        data: dataToSend,
+        contentType: "application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            $("#id").val("");
+            $("#name").val("");
+            $("#brand").val("");
+            $("#model").val("");
+            $("#category_id").val("");
+            traerInformacion();    
+            alert("Se ha actualizado");
+        }
+    });
+}
+function borrarElemento(idElemento)
+{
+    let myData = {
+        id:idElemento 
+    }
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"https://g4ef86a70a56228-db202109252211.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/skate/skate",
+        type: "DELETE",
+        data: dataToSend,
+        contentType: "application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            traerInformacion();    
+            alert("Se ha eliminado");
         }
     });
 }
